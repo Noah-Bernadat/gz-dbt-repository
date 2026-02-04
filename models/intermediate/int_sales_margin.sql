8571e0491c43c92171f7fcc333d28179aab39ff1
Noah Bernadat <bernadatnoah.crea@gmail.com>
2026-02-04 15:43:51 +0000

    Calcul de la marge par produit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,5 @@
 select
+    s.date_date,
     s.orders_id,
     s.product_id,
     s.quantity,
@@ -8,7 +9,7 @@ select
     s.revenue - (s.quantity * p.purchase_price) as margin
 from {{ source('stg_gz_raw_data', 'sales') }} s
 left join {{ source('stg_gz_raw_data', 'product') }} p
-    on s.product_id = p.product_id
+    on s.product_id = p.products_id
 
 
 
